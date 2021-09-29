c1ab5f3af3d7e96ba1e0b599c8635250ba284356
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-09-29 23:36:59 +0300

    Feat: ContentFooter component was created and used in Content component.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import ContentFooter from '../ContentFooter/ContentFooter'
 import ToDoList from '../ToDoList/ToDoList'
 
 const Content: React.FC = () => {
   return (
-    <section className="main">
-		<input className="toggle-all" type="checkbox" />
-		<label htmlFor="toggle-all">
-			Mark all as complete
-		</label>
-
-		<ToDoList />
-	</section>
+		<>
+			<section className="main">
+				<input className="toggle-all" type="checkbox" />
+				<label htmlFor="toggle-all">
+					Mark all as complete
+				</label>
+				<ToDoList />
+			</section>
+			<ContentFooter />
+		</>
   )
 }
 
