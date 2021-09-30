19e5f804d9f3377012a217d217dce388913d146b
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-09-30 23:02:23 +0300

    Feat: Add to do operations was created with useAppDispatch.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import { addToDo } from "../../redux/todos/todosSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
 const Form: React.FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(addToDo(
+      {
+        id: nanoid(),
+        title,
+        completed: false,
+      }
+    ))
+    setTitle("");
+  };
+
   return (
-    <form>
-      <input className="new-todo" placeholder="What needs to be done?" />
+    <form onSubmit={handleSubmit}>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
     </form>
-  )
-}
+  );
+};
 
 export default Form;
