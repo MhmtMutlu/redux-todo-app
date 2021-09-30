8ece8eba5e1f841a8a386921f335f91d63979c0f
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-09-30 23:21:33 +0300

    Feat-Conf: toggle reducer was defined.

--- src/redux/todos/todosSlice.ts
+++ src/redux/todos/todosSlice.ts
@@ -1,10 +1,14 @@
-import { createSlice, PayloadAction  } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type Items = {
   id: string;
   title: string;
   completed: boolean;
-}
+};
+
+type Id = {
+  id: string;
+};
 
 export const todosSlice = createSlice({
   name: "todos",
@@ -19,15 +23,21 @@ export const todosSlice = createSlice({
         id: "2",
         title: "Read Book",
         completed: false,
-      }
+      },
     ],
   },
   reducers: {
     addToDo: (state, action: PayloadAction<Items>) => {
-      state.items.push(action.payload)
-    }
+      state.items.push(action.payload);
+    },
+    toggle: (state, action: PayloadAction<Id>) => {
+      const { id } = action.payload;
+      const item = state.items.find((obj) => obj.id === id);
+
+      item && (item.completed = !item?.completed);
+    },
   },
 });
 
-export const { addToDo } = todosSlice.actions
+export const { addToDo, toggle } = todosSlice.actions;
 export default todosSlice.reducer;
