efe439e2865d661ae676b6a87186c81b2cf745e5
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-09-30 23:22:18 +0300

    Feat: Toggle operations was created to change status of todo with useAppDispatch.

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { useAppSelector } from '../../hooks/reduxHooks';
+import React from "react";
+import { useAppSelector, useAppDispatch } from "../../hooks/reduxHooks";
+import { toggle } from "../../redux/todos/todosSlice";
 
 const ToDoList: React.FC = () => {
-	const items = useAppSelector(state => state.todos.items);
+  const dispatch = useAppDispatch();
+  const items = useAppSelector((state) => state.todos.items);
 
   return (
     <ul className="todo-list">
-			{
-				items.map((item) => (
-					<li key={item.id} className={item.completed ? "completed" : ""}>
-						<div className="view">
-							<input className="toggle" type="checkbox" />
-							<label>{item.title}</label>
-							<button className="destroy"></button>
-						</div>
-					</li>
-				))
-			}
-		</ul>
-  )
-}
+      {items.map((item) => (
+        <li key={item.id} className={item.completed ? "completed" : ""}>
+          <div className="view">
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
+            <label>{item.title}</label>
+            <button className="destroy"></button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 export default ToDoList;
