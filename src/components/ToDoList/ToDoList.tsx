75c6c3e37fb70914b6a5ce067fdfb40e22afe2e2
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-01 22:40:08 +0300

    Feat: activeFilter func was created to filter todo items.

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -2,13 +2,32 @@ import React from "react";
 import { useAppSelector, useAppDispatch } from "../../hooks/reduxHooks";
 import { toggle, destroy } from "../../redux/todos/todosSlice";
 
+type ToDoItem = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+let filtered: Array<ToDoItem> = [];
+
 const ToDoList: React.FC = () => {
   const dispatch = useAppDispatch();
   const items = useAppSelector((state) => state.todos.items);
+  const activeFilter = useAppSelector((state) => state.todos.activeFilter);
+
+  if (activeFilter !== "all") {
+    filtered = items.filter((todo) =>
+      activeFilter === "active"
+        ? todo.completed === false
+        : todo.completed === true
+    );
+  } else {
+    filtered = items;
+  }
 
   return (
     <ul className="todo-list">
-      {items.map((item) => (
+      {filtered.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
             <input
