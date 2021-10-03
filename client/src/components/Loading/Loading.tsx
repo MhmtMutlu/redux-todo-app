5ec5b97a62d87e4dcf93ad4e19050ec16eaba678
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-03 15:05:26 +0300

    UI: Loading animation component was created.

---
+++ client/src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className="lds-wrapper">
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+};
+
+export default Loading;
